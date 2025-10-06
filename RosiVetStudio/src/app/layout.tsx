a851113f2cfc0443dcb66b2c3fe2c250d08f6b20
Jfscastro88 <joaofscastro@gmail.com>
2025-10-06 10:10:50 +0200

    vercel insights

--- RosiVetStudio/src/app/layout.tsx
+++ RosiVetStudio/src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import Providers from "./providers";
 import { NavbarClient } from "./components/Navbar";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -25,6 +26,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <div className="h-15 sm:h-20"></div>
           {children}
         </Providers>
+        <SpeedInsights />
       </body>
     </html>
   );
